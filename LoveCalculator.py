a9b72a17b874a248d53484a6311a064c630a84c2
Rutendo Musuka <musuka604@gmail.com>
2022-09-05 09:38:21 -0700

    Bring back a classic to use for match making

---
+++ LoveCalculator.py
@@ -0,0 +1,23 @@
+# Author : Rutendo Musuka
+# Purpose :Bringing the old times back - Calculates your potential love partner using your names
+#Source: https://www.buzzfeed.com/ariannarebolini/what-are-the-chances-your-crush-is-actually-your-true-love
+#If buzzfeed says it works, who am I to question it
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+combined_name = name1.lower() + name2.lower()
+true= "true"
+love="love"
+total_matches_in_true = total_matches_in_love = 0
+for letter in true:
+    match =  combined_name.count(letter)
+    total_matches_in_true += match
+
+for letter in love:
+    match =  combined_name.count(letter)
+    total_matches_in_love += match
+
+love_score = total_matches_in_true*10 + total_matches_in_love
+
+print(f"Your love is {love_score}%")
+
