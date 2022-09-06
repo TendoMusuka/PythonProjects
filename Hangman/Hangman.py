99602e725e12b7ca831a41894a2c557884f72aee
Rutendo Musuka <musuka604@gmail.com>
2022-09-05 20:44:28 -0700

    Get to play hangman!!

---
+++ Hangman/Hangman.py
@@ -0,0 +1,42 @@
+import random
+from hangman_art import stages, logo
+from hangman_words import word_list
+
+print(logo)
+game_is_finished = False
+lives = len(stages) - 1
+print(f"You have {lives} lives.")
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not game_is_finished:
+    guess = input("Guess a letter: ").lower()
+
+    # Use the clear() function imported from replit to clear the output between guesses.
+
+    if guess in display:
+        print(f"You've already guessed {guess}")
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(f"{' '.join(display)}")
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.You have {lives-1} lives left")
+        lives -= 1
+        if lives == 0:
+            game_is_finished = True
+            print("You lose.")
+            print(f"The word was {chosen_word}")
+
+    if not "_" in display:
+        game_is_finished = True
+        print("You win.")
+
+    print(stages[lives])
